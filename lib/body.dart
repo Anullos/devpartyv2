f60a9fdb146eed0d7aa91454dd68b77607f29215
Anullos <daranda@smartdream.es>
2024-11-22 13:28:39 +0100

    feat: update body

--- lib/body.dart
+++ lib/body.dart
@@ -6,34 +6,54 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Responsive.isMobile(context)
-          ? ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) => Container(
-                margin: const EdgeInsets.only(bottom: 8),
-                color: Colors.grey[300],
-                child: ListTile(
-                  title: Text('Item ${index + 1}'),
-                  onTap: () {},
-                ),
-              ),
-            )
-          : GridView.count(
-              crossAxisCount: Responsive.isDesktop(context) ? 5 : 4,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-              children: List.generate(
-                20,
-                (index) => Container(
-                  color: Colors.grey[300],
-                  child: Center(
-                    child: Text('Item ${index + 1}'),
-                  ),
-                ),
-              ),
-            ),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Responsive(
+        mobile: BodyMobile(),
+        tablet: BodyDesktop(),
+        desktop: BodyDesktop(),
+      ),
+    );
+  }
+}
+
+class BodyMobile extends StatelessWidget {
+  const BodyMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) => Container(
+        margin: const EdgeInsets.only(bottom: 8),
+        color: Colors.grey[300],
+        child: ListTile(
+          title: Text('Item ${index + 1}'),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class BodyDesktop extends StatelessWidget {
+  const BodyDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: Responsive.isDesktop(context) ? 5 : 4,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      children: List.generate(
+        20,
+        (index) => Container(
+          color: Colors.grey[300],
+          child: Center(
+            child: Text('Item ${index + 1}'),
+          ),
+        ),
+      ),
     );
   }
 }
